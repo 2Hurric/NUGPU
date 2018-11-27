9a06b54dd5828188ead6da8602da121484b52d1a
2Hurric <zyk100yyy@gmail.com>
2018-11-26 22:17:49 -0500

    sparse histo

--- src/heq.cu
+++ src/heq.cu
@@ -27,6 +27,7 @@
 #define DEBUG
 #define HIST_SIZE 256
 #define SCAN_SIZE HIST_SIZE*2
+#define WARP_SIZE 32
 
 unsigned char *input_gpu;
 unsigned char *output_gpu;
@@ -57,10 +58,16 @@ inline cudaError_t checkCuda(cudaError_t result) {
 __global__ void kernel_hist(unsigned char *input, 
                        float *hist, unsigned int height, unsigned int width){
 
-    int x = blockIdx.x*TILE_SIZE+threadIdx.x;
-    int y = blockIdx.y*TILE_SIZE+threadIdx.y;
+    //int x = blockIdx.x*TILE_SIZE+threadIdx.x;
+    //int y = blockIdx.y*TILE_SIZE+threadIdx.y;
+    int interval_x = (width - 1) / TILE_SIZE + 1;
+    int interval_y = (height - 1) / TILE_SIZE + 1;
+    int y = threadIdx.y*interval_y+blockIdx.y;
+    int x = threadIdx.x*interval_x+blockIdx.x;
+    //int location =  y*TILE_SIZE*gridDim.x+x;
+    //int location =  y*width+x;
+    int location = y*width + x;
 
-    int location =  y*TILE_SIZE*gridDim.x+x;
     int block_loc = threadIdx.y*TILE_SIZE+threadIdx.x;
     // HIST_SIZE 256
     __shared__ unsigned int hist_shared[HIST_SIZE];
@@ -76,6 +83,73 @@ __global__ void kernel_hist(unsigned char *input,
     //if (block_loc<HIST_SIZE) {
     atomicAdd(&(hist[block_loc]),(float)hist_shared[block_loc]);
     //}
+}
+
+__global__ void kernel_hist3(unsigned char *input, 
+                       float *hist, unsigned int height, unsigned int width){
+
+    int x = blockIdx.x*TILE_SIZE+threadIdx.x;
+    int y = blockIdx.y*TILE_SIZE+threadIdx.y;
+    // int interval_x = (width - 1) / TILE_SIZE + 1;
+    // int interval_y = (height - 1) / TILE_SIZE + 1;
+    // int y = threadIdx.y*interval_y+blockIdx.y;
+    // int x = threadIdx.x*interval_x+blockIdx.x;
+    // int location =  y*TILE_SIZE*gridDim.x+x;
+    // int location =  y*width+x;
+    int location = y*width + x;
+
+    int block_loc = threadIdx.y*TILE_SIZE+threadIdx.x;
+    // HIST_SIZE 256
+    __shared__ unsigned int hist_shared[HIST_SIZE];
+
+    /*if (block_loc<HIST_SIZE) hist_shared[block_loc]=0;
+    __syncthreads();*/
+    hist_shared[block_loc]=0;
+    __syncthreads();
+
+    if (x<width && y<height) atomicAdd(&(hist_shared[input[location]]),1);
+    __syncthreads();
+
+    //if (block_loc<HIST_SIZE) {
+    atomicAdd(&(hist[block_loc]),(float)hist_shared[block_loc]);
+    //}
+}
+
+__global__ void kernel_hist2 (float*histo, unsigned char* data,int size,int BINS,int R){
+
+  __shared__ int Hs[2048]; //(BINS+1*)R
+
+  //Warp index
+  const int warpid=(int)(threadIdx.x / WARP_SIZE);
+  const int lane=threadIdx.x % WARP_SIZE;
+  const int warps_block=blockDim.x /WARP_SIZE;
+
+  // Offset to per-block
+  const int off_rep = (BINS +1) * (threadIdx.x % R);
+
+  //Constants for interleaved read access
+  const int begin = (size / warps_block) * warpid + WARP_SIZE * blockIdx.x +lane;
+  const int end = (size / warps_block) * (warpid+1);
+  const int step = WARP_SIZE * gridDim.x;
+
+  //Initializetion
+  for (int pos =threadIdx.x; pos< (BINS+1)*R;pos+=blockDim.x) Hs[pos]=0;
+
+  __syncthreads();
+
+  for (int i=begin;i<end;i+=step){
+    atomicAdd(&Hs[off_rep+data[i]],1);
+  }
+
+  __syncthreads();
+
+  //Merge
+  for (int pos=threadIdx.x;pos<BINS;pos+=blockDim.x){
+    int sum=0;
+    for (int base =0;base<(BINS+1)*R;base+=BINS+1)
+      sum+=Hs[base+pos];
+    atomicAdd(histo +pos, sum);
+  }
 }
 
 __global__ void kernel_hist_global(unsigned char *input, 
@@ -156,6 +230,9 @@ void histogram_gpu(unsigned char *data,
     
     // Both are the same size (CPU/GPU).
     int size = XSize*YSize;
+
+    int block_size2=256;
+    int grid_size2=1+((width*height-1) / block_size2);
     
     // Allocate arrays in GPU memory
     float Ktime00;
@@ -184,6 +261,10 @@ void histogram_gpu(unsigned char *data,
 
     dim3 dimGrid(gridXSize, gridYSize);
     dim3 dimBlock(TILE_SIZE, TILE_SIZE);
+    //hist2
+    dim3 dimGrid2(grid_size2);
+    dim3 dimBlock2(block_size2);
+
     dim3 dimCdfGrid(1);
     dim3 dimCdfBlock(HIST_SIZE);
 
@@ -197,10 +278,15 @@ void histogram_gpu(unsigned char *data,
         float Ktime0;
         TIMER_CREATE(Ktime0);
         TIMER_START(Ktime0);
-        kernel_hist<<<dimGrid, dimBlock>>>(input_gpu, 
-                                      hist_array,
-                                      height,
-                                      width);
+        // kernel_hist<<<dimGrid, dimBlock>>>(input_gpu, 
+        //                               hist_array,
+        //                               height,
+        //                               width);
+       kernel_hist2<<<dimGrid2, dimBlock2>>>(hist_array,
+                                         input_gpu,
+                                         size,
+                                         255,
+                                         8);
         checkCuda(cudaPeekAtLastError());                                     
         checkCuda(cudaDeviceSynchronize());
         TIMER_END(Ktime0);
@@ -284,10 +370,18 @@ void histogram_gpu_warmup(unsigned char *data,
     
     // Both are the same size (CPU/GPU).
     int size = XSize*YSize;
+
+    int block_size2=256;
+    int grid_size2=1+((width*height-1) / block_size2);
     
     // Allocate arrays in GPU memory
+    float Ktime00;
+    TIMER_CREATE(Ktime00);
+    TIMER_START(Ktime00);
     cudaMalloc((void**)&input_gpu   , size*sizeof(unsigned char));
     cudaMalloc((void**)&hist_array  , HIST_SIZE*sizeof(float));
+    TIMER_END(Ktime00);
+    //printf("CUDA_MALLOC Execution Time: %f ms\n", Ktime00);
     // cudaMalloc((void**)&output_gpu  , size*sizeof(unsigned char));
     // checkCuda(cudaMalloc((void**)&cdf         , size*sizeof(float)));
     // init output_gpu to 0
@@ -307,27 +401,47 @@ void histogram_gpu_warmup(unsigned char *data,
 
     dim3 dimGrid(gridXSize, gridYSize);
     dim3 dimBlock(TILE_SIZE, TILE_SIZE);
-    dim3 dimCdfGrid(1 + (size - 1) / HIST_SIZE);
+    //hist2
+    dim3 dimGrid2(grid_size2);
+    dim3 dimBlock2(block_size2);
+
+    dim3 dimCdfGrid(1);
     dim3 dimCdfBlock(HIST_SIZE);
 
     // Kernel Call
-
+    #if defined(CUDA_TIMING)
+        float Ktime;
+        TIMER_CREATE(Ktime);
+        TIMER_START(Ktime);
+    #endif
         
-
+        float Ktime0;
+        TIMER_CREATE(Ktime0);
+        TIMER_START(Ktime0);
         kernel_hist<<<dimGrid, dimBlock>>>(input_gpu, 
                                       hist_array,
                                       height,
                                       width);
+        // kernel_hist2<<<dimGrid2, dimBlock2>>>(hist_array,
+        //                                  input_gpu,
+        //                                  size,
+        //                                  255,
+        //                                  8);
         checkCuda(cudaPeekAtLastError());                                     
         checkCuda(cudaDeviceSynchronize());
+        TIMER_END(Ktime0);
+        //printf("HIST Kernel Execution Time: %f ms\n", Ktime0);
 
-
+        float Ktime1;
+        TIMER_CREATE(Ktime1);
+        TIMER_START(Ktime1);
         kernel_cdf<<<dimCdfGrid,dimCdfBlock>>>( 
                                       hist_array,
                                       height*width);
         checkCuda(cudaPeekAtLastError());                                     
         checkCuda(cudaDeviceSynchronize());
-
+        TIMER_END(Ktime1);
+        //printf("CDF Kernel Execution Time: %f ms\n", Ktime1);
    
 
 // ///////////////////////////////////////////        
@@ -348,6 +462,11 @@ void histogram_gpu_warmup(unsigned char *data,
 // ///////////////////////////////////////////
 
 
+
+        float Ktime2;
+        TIMER_CREATE(Ktime2);
+        TIMER_START(Ktime2);
+
         kernel_equlization<<<dimGrid, dimBlock>>>(
                                       input_gpu,
                                       hist_array, 
@@ -356,8 +475,13 @@ void histogram_gpu_warmup(unsigned char *data,
 
         checkCuda(cudaPeekAtLastError());                                     
         checkCuda(cudaDeviceSynchronize());
-
+        TIMER_END(Ktime2);
+        //printf("EQUALIZATION Kernel Execution Time: %f ms\n", Ktime2);
     
+    #if defined(CUDA_TIMING)
+        TIMER_END(Ktime);
+        //printf("Kernel Execution Time: %f ms\n", Ktime);
+    #endif
         
     // Retrieve results from the GPU
     checkCuda(cudaMemcpy(data, 
